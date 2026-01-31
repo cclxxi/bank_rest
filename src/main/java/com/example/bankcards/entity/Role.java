88df3569ad665cbb2d03a4ccdef23d5f658779d5
cclxxi <ilya.proshin82@gmail.com>
2026-01-31 11:13:10 +0600

    feat(entity): add `Role` entity with JPA annotations
    - Introduced the `Role` entity with fields for `id`, `name`, associated `users`, and timestamps.
    - Configured lifecycle callbacks for `createdAt` and `updatedAt` timestamps.

---
+++ src/main/java/com/example/bankcards/entity/Role.java
@@ -0,0 +1,40 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false, length = 20)
+    private String name;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    private Set<User> users;
+
+    @Column(name = "created_at", unique = false, nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", unique = false, nullable = false, updatable = true)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
