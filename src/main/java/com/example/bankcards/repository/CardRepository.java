4bb0d72b7992196bdf13f5a64e323820d6f4bace
cclxxi <ilya.proshin82@gmail.com>
2026-01-31 14:14:10 +0600

    feat(repository): add `CardRepository` for card-related database operations
    - Introduced methods to find cards by user ID and filter by status.

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,15 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.enums.CardStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    List<Card> findByAccount_User_Id(Long userId);
+
+    List<Card> findByAccount_User_IdAndStatus(Long account_user_id, CardStatus status);
+
+}
