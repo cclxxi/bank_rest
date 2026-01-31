d00e457b8e23feb904d136075ab27eab55c8d06c
cclxxi <ilya.proshin82@gmail.com>
2026-01-31 14:17:34 +0600

    feat(service): add `UserService` interface for user operations
    - Defined methods for user profile retrieval, user management, and user state updates.

---
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.AdminUserDTO;
+import com.example.bankcards.dto.AdminUserSummaryDTO;
+import com.example.bankcards.dto.UserProfileDTO;
+
+import java.util.List;
+
+public interface UserService {
+    UserProfileDTO getCurrentUserProfile();
+    AdminUserDTO getUserById();
+    List<AdminUserSummaryDTO> getAllUsers();
+    void activateUser(Long userId);
+    void blockUser(Long userId);
+}
