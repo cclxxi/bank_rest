0dee33d2ef05e69f50df963a43c374ce297d39f9
cclxxi <ilya.proshin82@gmail.com>
2026-02-01 15:16:37 +0600

    feat(security): add `CustomUserDetails` for Spring Security integration
    - Implemented `CustomUserDetails` to adapt the `User` entity for Spring Security's `UserDetails` interface.

---
+++ src/main/java/com/example/bankcards/util/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package com.example.bankcards.util;
+
+import com.example.bankcards.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    @Getter
+    private final Long userId;
+    private final String login;
+    private final String password;
+    private final String role;
+    private final boolean active;
+
+    public CustomUserDetails(User user) {
+        this.userId = user.getId();
+        this.login = user.getEmail();
+        this.password = user.getPasswordHashed();
+        this.role = user.getRole().getName();
+        this.active = user.isActive();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
