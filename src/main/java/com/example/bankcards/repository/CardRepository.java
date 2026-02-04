84a5e29b5c95989362a6d9cd75675fa4e5ddcbe9
cclxxi <ilya.proshin82@gmail.com>
2026-02-04 11:22:35 +0600

    feat(repo): enhance repository methods
    - Added `findByAccount_User_IdWithSearch` method in `CardRepository` for paginated search functionality.
    - Introduced `AdminUserSummaryDTO` and pagination support in `UserRepository`.

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -2,13 +2,19 @@ package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.Card;
 import com.example.bankcards.enums.CardStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
     List<Card> findByAccount_User_Id(Long userId);
+
+    @Query("SELECT c FROM Card c WHERE c.account.user.id = :userId AND (:search IS NULL OR c.panMasked LIKE %:search%)")
+    Page<Card> findByAccount_User_IdWithSearch(Long userId, String search, Pageable pageable);
 
     List<Card> findByAccount_User_IdAndStatus(Long account_user_id, CardStatus status);
 
--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.bankcards.repository;
 
+import com.example.bankcards.dto.AdminUserSummaryDTO;
 import com.example.bankcards.entity.User;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
