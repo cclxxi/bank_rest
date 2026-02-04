13f8b9b2aeb14f290e208976f047715fe29e4e3c
cclxxi <ilya.proshin82@gmail.com>
2026-02-04 12:28:48 +0600

    feat(docs): add Swagger annotations to enums for improved API documentation
    - Annotated all enums (`CardStatus`, `UserStatus`, `TransactionType`, `TransactionStatus`, `AccountStatus`) with Swagger `@Schema` and field-level annotations for detailed descriptions.

--- src/main/java/com/example/bankcards/enums/TransactionType.java
+++ src/main/java/com/example/bankcards/enums/TransactionType.java
@@ -1,7 +1,13 @@
 package com.example.bankcards.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Тип транзакции")
 public enum TransactionType {
+    @Schema(description = "Пополнение")
     DEPOSIT,
+    @Schema(description = "Снятие")
     WITHDRAWAL,
+    @Schema(description = "Перевод")
     TRANSFER
 }
